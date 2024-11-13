66165f6befce64ac745e8cc4b68e74436684cedb
Rakgiri <133025494+Rakgiri@users.noreply.github.com>
2024-11-13 15:48:52 +0530

    Update cicd_queue.tf

--- modules/routing_queue/cicd_queue.tf
+++ modules/routing_queue/cicd_queue.tf
@@ -2,7 +2,7 @@
 resource "genesyscloud_routing_queue" "cicd_queue" {
   name                              = "CICD Queue NNI"
   division_id						            = var.cicd_division
-  description                       = "This is a CICD Check  Queue"
+  description                       = "This is a CICD Check Queue"
   acw_wrapup_prompt                 = "MANDATORY_TIMEOUT"
   acw_timeout_ms                    = 400000
   skill_evaluation_method           = "BEST"
